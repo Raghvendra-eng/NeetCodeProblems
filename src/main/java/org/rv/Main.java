283124b6bbb6bb9ca00d17fecfadc265d830c324
Raghvendra Pandey <raghvendrapandey@Raghvendras-MacBook-Air.local>
2025-03-20 05:09:09 +0530

    initial project setup

---
+++ src/main/java/org/rv/Main.java
@@ -0,0 +1,20 @@
+package org.rv;
+
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Below list contains ProblemCodes and problem description. Choose the ProblemCode from the below list");
+        System.out.println("\nNeetCode Arrays & Hashing Problems List:\n");
+        System.out.println("AHP-1: Contains Duplicate");
+        System.out.print("Enter problem code : ");
+
+        Scanner sc = new Scanner(System.in);
+        String problemCode = sc.next();
+
+        switch (problemCode) {
+            case "AHP-1":
+                break;
+        }
+    }
+}
