1ec13c7a72d91173461cb39a5836110111aec2b5
Raghvendra Pandey <raghvendrapandey@Raghvendras-MacBook-Air.local>
2025-03-21 07:14:50 +0530

    two sum solution

--- src/main/java/org/rv/Main.java
+++ src/main/java/org/rv/Main.java
@@ -1,6 +1,7 @@
 package org.rv;
 
 import org.rv.solution.ContainsDuplicate;
+import org.rv.solution.TwoSum;
 import org.rv.solution.ValidAnagram;
 
 import java.util.*;
@@ -11,6 +12,7 @@ public class Main {
         System.out.println("\nNeetCode Arrays & Hashing Problems List:\n");
         System.out.println("AHP-1: Contains Duplicate");
         System.out.println("AHP-2: Valid Anagram");
+        System.out.println("AHP-3: Two Sum");
         System.out.print("\nEnter problem code : ");
 
         Scanner sc = new Scanner(System.in).useDelimiter("\n");
@@ -22,11 +24,7 @@ public class Main {
                 System.out.println("Please provide comma separated integer values of the array (with space after comma)");
                 System.out.println("eg. 1, 2, 3, 4");
                 String inputArray = sc.next();
-                String[] stringNumber = inputArray.split(", ");
-                int []nums = new int[stringNumber.length];
-                for (int iterator = 0; iterator < stringNumber.length; iterator++) {
-                    nums[iterator] = Integer.parseInt(stringNumber[iterator]);
-                }
+                int []nums = getIntegerArrayFromString(inputArray);
                 ContainsDuplicate obj = new ContainsDuplicate();
                 System.out.println(obj.hasDuplicate(nums));
                 break;
@@ -40,6 +38,34 @@ public class Main {
                 ValidAnagram validAnagram = new ValidAnagram();
                 System.out.println(validAnagram.isAnagram(first, second));
                 break;
+            case "AHP-3":
+                System.out.println("Two Sum");
+                System.out.println("Please provide comma separated integer values of the array (with space after comma)");
+                System.out.println("eg. 1, 2, 3, 4");
+                String inputArrayForTwoSum = sc.next();
+                System.out.println("Provide target sum");
+                int target = sc.nextInt();
+                TwoSum twoSumObj = new TwoSum();
+                int []ans = twoSumObj.twoSum(getIntegerArrayFromString(inputArrayForTwoSum), target);
+                System.out.printf("Index that sum up target = %d\n", target);
+                printArray(ans);
+                break;
         }
+    }
+
+    private static int[] getIntegerArrayFromString(String inputArray) {
+        String[] stringNumber = inputArray.split(", ");
+        int []nums = new int[stringNumber.length];
+        for (int iterator = 0; iterator < stringNumber.length; iterator++) {
+            nums[iterator] = Integer.parseInt(stringNumber[iterator]);
+        }
+        return nums;
+    }
+
+    private static void printArray(int []nums) {
+        for ( int num: nums) {
+            System.out.print(num + " ");
+        }
+        System.out.println();
     }
 }
---
+++ src/main/java/org/rv/solution/TwoSum.java
@@ -0,0 +1,30 @@
+package org.rv.solution;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/*
+Given an array of integers nums and an integer target,
+return the indices i and j such that nums[i] + nums[j] == target and i != j.
+
+You may assume that every input has exactly one pair of indices i and j that satisfy the condition.
+
+Return the answer with the smaller index first.
+ */
+public class TwoSum {
+
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        int[] ans =  new int[]{-1, -1};
+        for (int i = 0; i < nums.length; i++) {
+            if (map.containsKey(target - nums[i])) {
+                ans[0] = map.get(target - nums[i]);
+                ans[1] = i;
+                return ans;
+            }
+            map.put(nums[i], i);
+        }
+        return ans;
+    }
+
+}
