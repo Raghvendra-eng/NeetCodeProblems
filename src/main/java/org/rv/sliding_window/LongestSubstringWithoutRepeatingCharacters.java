e55696d4693bb2a242ddc4fe67924c5954c6a8e4
Raghvendra Pandey <raghvendrapandey@Raghvendras-MacBook-Air.local>
2025-03-23 09:55:11 +0530

    best time to buy and sell stock solution

---
+++ src/main/java/org/rv/sliding_window/LongestSubstringWithoutRepeatingCharacters.java
@@ -0,0 +1,19 @@
+package org.rv.sliding_window;
+
+/**
+ * Given a string s, find the length of the longest substring without duplicate characters.
+ *
+ * A substring is a contiguous sequence of characters within a string.
+ *
+ * Example 1:
+ *
+ * Input: s = "zxyzxyz"
+ *
+ * Output: 3
+ */
+public class LongestSubstringWithoutRepeatingCharacters {
+    public int lengthOfLongestSubstring(String s) {
+        int ans = 0;
+        return ans;
+    }
+}
